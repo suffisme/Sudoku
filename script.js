c9f0b3d04541f7f1d605f4c3ffe23c2e0a3b8237
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2023-01-06 20:30:07 +0530

    Change in logics

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const SODUKO = [
     [null,null,null,null,null,null,null,null,null]
 ];
 const BORDER_WIDTH = '3px';
+const START_CELL = 'c55';
+const DIFFICULTY = ['Easy', 'Medium', 'Hard']
 
 const COMMAND_KEYS = [
     'Control', 
@@ -24,13 +26,26 @@ const COMMAND_KEYS = [
     'NumLock'
 ]
 
+BG_COLORS = ['rgba(51,51,51,0.3)','white']
+PAUSE_RESUME_COMMAND = ["Resume", "Pause"]
+
+const CELLS_FILLED_UTIL = [45, 35, 25]
+
 /*===============================================*/
 
-var currentCell = 'c55';
-var cellR = 4;
-var cellC = 4;
+var GAME_RUN = false
+var chosenDifficulty = 0
+var currentCell = START_CELL;
+var cellR = START_CELL[1] - 1;
+var cellC = START_CELL[2] - 1;
 var isError = false;
 var count = 0;
+var cellsToBeFilled = CELLS_FILLED_UTIL[chosenDifficulty];
+const selectedCells = []
+
+var sodukoBG = document.getElementById('sudoku-bg')
+var pauseResumeBtn = document.getElementById('pause-resume')
+var restartBtn = document.getElementById('restart')
 
 /*===============================================*/
 
@@ -53,6 +68,12 @@ const makeBGWhite = () => {
 }
 
 const makeCurrent = (id) => {
+
+    if(!GAME_RUN)
+    {
+        if('c'+id != START_CELL)
+            return;
+    }
     if(!isError)
         makeBGWhite();
     else 
@@ -64,6 +85,12 @@ const makeCurrent = (id) => {
     HighLightCurrent();    
     else
     document.getElementById(currentCell).style.background = MAIN_CELL_COLOR
+}
+
+const priorityCells = () => {
+    selectedCells.forEach((cell) => {
+
+    })
 }
 
 const HighLightCurrent = () => {
@@ -110,22 +137,28 @@ const checkForError = () => {
             }
         }
     }
-    var pR = parseInt((cellR)/3);
-    var pC = parseInt((cellC)/3);
+    for(var ii = 0; ii < 3 ; ii++)
+    {
+        var pR = ii;
+        for(var jj = 0; jj < 3; jj++)
+        {
+            var pC = jj;
 
-    const vR = [pR*3, pR*3 + 1, pR*3 + 2]
-    const vC = [pC*3, pC*3 + 1, pC*3 + 2]
+            const vR = [pR*3, pR*3 + 1, pR*3 + 2]
+            const vC = [pC*3, pC*3 + 1, pC*3 + 2]
 
-    const checkBOX = []
-    for(var i=0; i<3;i++){
-        for(var j=0; j<3;j++){
-            if(SODUKO[vR[i]][vC[j]])
-            {
-                if(checkBOX.indexOf(SODUKO[vR[i]][vC[j]]) > -1)
-                    return true;
-                checkBOX.push(SODUKO[vR[i]][vC[j]])
-            }
-        }
+            const checkBOX = []
+            for(var i=0; i<3;i++){
+                for(var j=0; j<3;j++){
+                    if(SODUKO[vR[i]][vC[j]])
+                    {
+                        if(checkBOX.indexOf(SODUKO[vR[i]][vC[j]]) > -1)
+                            return true;
+                        checkBOX.push(SODUKO[vR[i]][vC[j]])
+                    }
+                }
+            } 
+        }  
     }
     return false;
 }
@@ -140,16 +173,14 @@ const getCount = () => {
 }
 
 window.addEventListener('keydown', (e) => {
+    if(!GAME_RUN)
+        return;
+    var reserved = false;
+    if(selectedCells.indexOf(cellR*9+cellC) > -1)
+        reserved = true;
     const val = e.key;
-    console.log(val);
     if(COMMAND_KEYS.includes(val))
         return
-    if(val == 'Backspace' || val == 'Delete')
-    {
-        document.getElementById('error').innerHTML = ""
-        SODUKO[cellR][cellC] = null;
-        document.getElementById(('c'+(cellR+1))+(cellC+1)).innerHTML = "";
-    }
     else if(val == 'ArrowLeft' || val == 'ArrowRight' || val == 'ArrowUp' || val == 'ArrowDown')
     {
         document.getElementById('error').innerHTML = ""
@@ -165,16 +196,25 @@ window.addEventListener('keydown', (e) => {
         makeCurrent(cellCode)
         return;
     }
+    else if(reserved)
+        return;
+    else if(val == 'Backspace' || val == 'Delete')
+    {
+        document.getElementById('error').innerHTML = ""
+        SODUKO[cellR][cellC] = null;
+        document.getElementById(('c'+(cellR+1))+(cellC+1)).innerHTML = "";
+    }
     else if(val >= 1 && val <= 9 )
     {
         document.getElementById('error').innerHTML = ""
         document.getElementById(('c'+(cellR+1))+(cellC+1)).innerHTML = val;
-        SODUKO[cellR][cellC] = val;
+        SODUKO[cellR][cellC] = (val - 1) + 1;
     }
     else{
         document.getElementById('error').innerHTML = "Please Enter Valid Number"
     }
     var errorCheck = checkForError();
+
     if(isError !== errorCheck)
         makeBGWhite();
     isError = errorCheck;
@@ -186,15 +226,106 @@ window.addEventListener('keydown', (e) => {
     if(count == 81 && !isError)
     {
         document.getElementById('success').innerHTML = "Congratulations! You Completed The SODUKO"
+        GAME_RUN = false
     }
     document.getElementById('count').innerHTML = count
-})
 
-checkForError();
+    console.log(SODUKO)
+})
 
 const restartGame = () => {
     if(window.confirm("Are you sure you want to restart it?\nYour progress will be lost."))
     {
         window.location.reload();
     }
+}
+
+/* ===================== */
+document.getElementsByName('difficulty').forEach(function(chk){
+    chk.addEventListener('click', function() {
+        if(this.checked){
+        chosenDifficulty = this.value;
+        // console.log('DIFFICULTY', DIFFICULTY[chosenDifficulty])
+        }
+    });
+});
+
+const deactivateForm = () => {
+    document.getElementsByName('difficulty').forEach(function(chk){
+        chk.disabled = true
+    });
+    document.getElementById("submit-btn").disabled = true
+    document.getElementById("difficulty-form").style.color = 'grey'
+}
+
+const setDifficulty = (difficulty) => {
+    const difficultyTag = document.getElementById("diff")
+    difficultyTag.innerHTML = `(${difficulty})`
+
+    cellsToBeFilled = CELLS_FILLED_UTIL[chosenDifficulty];
+
+}
+
+const selectCells = () => {
+    const selectedCells_t = []
+    while(selectedCells_t.length !== cellsToBeFilled)
+    {
+        const data = (parseInt(Math.random() * (81 + 5)))%81;
+        if(selectedCells_t.indexOf(data) === -1)
+            selectedCells_t.push(data)
+    }
+    return selectedCells_t;
+}
+
+const fillCells = (selectedCells_t) => {
+    for(var j=0;j<selectedCells_t.length;j++){
+        const data = selectedCells_t[j];
+        const r = parseInt(data/9);
+        const c = data%9;
+        var val = -1;
+        for(var i=1; i < 10 ; i++) {
+            SODUKO[r][c] = i;
+            // console.log(i)
+            if(!checkForError())
+            {
+                val = i;
+                break;
+            }
+        }
+        if(val !== -1)
+        {
+            SODUKO[r][c] = val;
+            const cellId = 'c'+(r+1)+''+(c+1);
+            document.getElementById(cellId).innerHTML = val;
+            document.getElementById(cellId).style.fontWeight = "bolder"
+            document.getElementById(cellId).style.background = "grey!important"
+            selectedCells.push(data);
+        }
+        else SODUKO[r][c] = null;
+    }
+}
+
+const initBoard = () => {
+    const selectedCells_t = selectCells();
+    // console.log(selectedCells_t);
+    fillCells(selectedCells_t);
+}
+
+var form = document.getElementById("difficulty-form");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    GAME_RUN = true;
+    setDifficulty(DIFFICULTY[chosenDifficulty]);
+    deactivateForm();
+    sodukoBG.style.backgroundColor = 'white'
+    restartBtn.style.display = 'block';
+    pauseResumeBtn.style.display = 'block';
+
+    initBoard();
+});
+
+const pauseGame = () => {
+    GAME_RUN = true^GAME_RUN;
+    sodukoBG.style.backgroundColor = BG_COLORS[GAME_RUN]
+    pauseResumeBtn.innerText = PAUSE_RESUME_COMMAND[GAME_RUN];
 }
